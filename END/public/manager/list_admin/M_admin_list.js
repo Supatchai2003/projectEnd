0178e5bed3a09a5b25f4842e1ef25bf0c2e619a2
Supatchai Nakyoy <156934883+Supatchai2003@users.noreply.github.com>
2025-10-19 06:23:55 +0700

    save

--- END/public/manager/list_admin/M_admin_list.js
+++ END/public/manager/list_admin/M_admin_list.js
@@ -6,7 +6,7 @@ async function loadAdmins() {
   container.innerHTML = "";
 
   try {
-    const res = await fetch(`${API_BASE}/admins?role=admin`);
+    const res = await fetch(`${API_BASE}/admin?role=admin`);
     const { success, data } = await res.json();
 
     if (!success || !data.length) {
