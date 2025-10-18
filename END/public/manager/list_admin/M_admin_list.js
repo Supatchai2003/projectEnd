8a0b0b8942ce68a5a4ecb048c1855524bfa95eee
Supatchai Nakyoy <156934883+Supatchai2003@users.noreply.github.com>
2025-10-19 06:37:21 +0700

    Update M_admin_list.js

--- END/public/manager/list_admin/M_admin_list.js
+++ END/public/manager/list_admin/M_admin_list.js
@@ -6,7 +6,7 @@ async function loadAdmins() {
   container.innerHTML = "";
 
   try {
-    const res = await fetch(`${API_BASE}/get-user/:id?role=admin`);
+    const res = await fetch(`${API_BASE}/get-user/?role=admin`);
     const { success, data } = await res.json();
 
     if (!success || !data.length) {
